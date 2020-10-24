198b654b560cd79b3585693a195daeebe132d1c7
kausarm <muhammadkausar01@gmail.com>
2020-10-24 22:39:11 +0700

    first commit

---
+++ precache-manifest.6d6d6ebf874e2ee914e91c846dca8c76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e28f8c675c84300ee1fc31085f913a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c3c4a7de77b885ceac8",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c3c4a7de77b885ceac8",
+    "url": "/static/js/main.5078cec2.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
